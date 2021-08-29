017f4123cc4c46836056c67369d31f856567ec8f
w-cahyanto <68521383+w-cahyanto@users.noreply.github.com>
2021-08-29 15:54:31 +0700

    2nd Commit
    add assets, animation, and movement.

---
+++ YuroqiGame/Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float speed;
+	Animator anim;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update()
+	{
+		float keyX = Input.GetAxisRaw ("Horizontal");
+		rb.velocity = new Vector2 (keyX * speed * Time.deltaTime, rb.velocity.y);
+
+		if (keyX > 0) 
+		{
+			anim.SetBool ("isRunning", true);
+			transform.rotation = Quaternion.Euler (0f, 0f, 0f);
+		}
+
+		else if (keyX < 0) {
+			anim.SetBool ("isRunning", true);
+			transform.rotation = Quaternion.Euler (0f, -180f, 0f);
+		} 
+
+		else 
+		{
+			anim.SetBool ("isRunning", false);
+		}
+	}
+}
