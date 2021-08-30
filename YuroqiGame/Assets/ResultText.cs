e1cdbaee8cce8136b443478551b81701524641e5
w-cahyanto <68521383+w-cahyanto@users.noreply.github.com>
2021-08-30 16:13:49 +0700

    4th Commit
    V1 Basic Gameplay System

---
+++ YuroqiGame/Assets/ResultText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ResultText : MonoBehaviour
+{
+    public HoleCollision holeCollision;
+    TextMeshProUGUI resultText;
+    string value;
+
+    void Start()
+    {
+        resultText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+
+        
+
+        if (holeCollision.countsValuableBox == 3){
+
+             resultText.text =  holeCollision.valueableBox[1].GetComponentInChildren<TextMeshPro>().text + "+" + holeCollision.valueableBox[2].GetComponentInChildren<TextMeshPro>().text + "=" + holeCollision.valueableBox[0].GetComponentInChildren<TextMeshPro>().text;
+        }
+    }
+}
